9581d208f4ae674d14eb678390ae166c74cc9f30
phw <937855602@qq.com>
2018-05-06 20:50:02 +0800

    提供跨域支持

--- src/main/java/xyz/frt/govern/common/JWTFilter.java
+++ src/main/java/xyz/frt/govern/common/JWTFilter.java
@@ -5,10 +5,13 @@ import io.jsonwebtoken.ExpiredJwtException;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.web.filter.AccessControlFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
@@ -53,6 +56,28 @@ public class JWTFilter extends AccessControlFilter {
             ae.printStackTrace();
         }
         return true;
+    }
+
+    /**
+     * 对跨域提供支持
+     * @param request req
+     * @param response res
+     * @return Boolean
+     * @throws Exception ex
+     */
+    @Override
+    protected boolean preHandle(ServletRequest request, ServletResponse response) throws Exception {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Access-control-Allow-Origin", httpServletRequest.getHeader("Origin"));
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,PATCH,DELETE");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", httpServletRequest.getHeader("Access-Control-Request-Headers"));
+        // 跨域时会首先发送一个option请求，这里我们给option请求直接返回正常状态
+        if (httpServletRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            httpServletResponse.setStatus(HttpStatus.OK.value());
+            return false;
+        }
+        return super.preHandle(request, response);
     }
 
     private void out(ServletResponse res, String content) {
